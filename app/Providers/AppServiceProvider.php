c47d975c818d8a043cb314db83b79b311e8c2d6e
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2026-02-24 21:33:17 +0300

    Enforce Https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production') || env('FORCE_HTTPS', false) || str_starts_with(env('APP_URL', ''), 'https://') || env('APP_ENV') === 'production') {
+        URL::forceScheme('https');
+    }
     }
 }
