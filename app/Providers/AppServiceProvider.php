f6db700f6c6f7e33c8d8e5c004d9e69211a4d44b
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2026-05-02 13:16:14 +0300

    Fix: File upload errors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (app()->environment('production') || env('FORCE_HTTPS', false) || str_starts_with(env('APP_URL', ''), 'https://') || env('APP_ENV') === 'production') {
-        URL::forceScheme('https');
-    }
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
