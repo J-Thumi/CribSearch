fcee124c9413cb817da346becb908bfc5af46ae6
Josphat Waweru Thumi <thumijosphat47@gmail.com>
2026-08-19 23:11:24 +0300

    Fix contact us

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,7 +9,7 @@ class ContactController extends Controller
 {
     public function index()
     {
-        return view('contact-us');
+        return view('contact');
     }
 
     public function store(Request $request)
